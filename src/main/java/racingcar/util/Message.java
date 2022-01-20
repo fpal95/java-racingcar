561a009cbf959f837d0a87eaa9459496a7d2e62b
Ji In Lee <loveted0225@naver.com>
2022-01-20 17:11:05 +0900

    feat(Message): 자동차 이름 5글자 초과시 에러메시지 추가

--- src/main/java/racingcar/util/Message.java
+++ src/main/java/racingcar/util/Message.java
@@ -13,5 +13,9 @@ public class Message {
     public static final String ASK_TRY_COUNT ="시도할 횟수는 몇 회인가요?";
     public static final String GAME_RESULT_MESSAGE="출력결과";
     public static final String WINNER_MESSAGE= "최종 우승자: ";
+<<<<<<< HEAD
 >>>>>>> 40e5086a3 (feat(Message): 사용자에게 보여지는 메시지 선언)
+=======
+    public static final String CAR_NAME_ERROR= "[ERROR] 자동차 이름은 5글자를 초과할 수 없습니다.";
+>>>>>>> a77f2853d (feat(Message): 자동차 이름 5글자 초과시 에러메시지 추가)
 }
