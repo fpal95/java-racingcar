a3409c54cb03fa2aa4d3be71814a120af26c49ef
Ji In Lee <loveted0225@naver.com>
2022-01-20 16:54:04 +0900

    feat(RandomGenerator): 랜덤값 생성 구현

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -3,11 +3,17 @@ package racingcar.util;
 import java.util.Random;
 
 public class RandomGenerator {
+<<<<<<< HEAD
 
     private static final int MAX_RANGE_VALUE = 10;
 
     public int generateRandomNumber() {
         Random random = new Random();
         return random.nextInt(MAX_RANGE_VALUE);
+=======
+    public int generateRandomNumber(){
+        Random random= new Random();
+        return random.nextInt(9);
+>>>>>>> 6d341c991 (feat(RandomGenerator): 랜덤값 생성 구현)
     }
 }
