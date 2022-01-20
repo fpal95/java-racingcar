8cc35ab10d0529af1b3165902f1599f65d952cc9
Ji In Lee <loveted0225@naver.com>
2022-01-20 17:10:43 +0900

    feat(Car): 자동차 이름 5글자 초과시 예외처리

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,6 +1,7 @@
 package racingcar.model;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import racingcar.util.Message;
 import racingcar.util.RandomGenerator;
 
@@ -29,12 +30,16 @@ public class Car {
             throw new IllegalArgumentException(Message.CAR_NAME_ERROR);
         }
 =======
+=======
+import racingcar.util.Message;
+>>>>>>> 761a6ea3e (feat(Car): 자동차 이름 5글자 초과시 예외처리)
 import racingcar.util.RandomGenerator;
 
 public class Car {
     private String carName;
     private String position;
     public Car(String carName){
+        validateCarNameLength(carName);
         this.carName= carName;
         this.position="";
 >>>>>>> ae90941b6 (feat(Car): 사용자 입력값에 따라 car 생성)
@@ -48,6 +53,11 @@ public class Car {
             this.position+="-";
         }
 
+    }
+    private void validateCarNameLength(String carName) {
+        if(carName.length()>5){
+            throw new IllegalArgumentException(Message.CAR_NAME_ERROR);
+        }
     }
 
     public String getCarName() {
