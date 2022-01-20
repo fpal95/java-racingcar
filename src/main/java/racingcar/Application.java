df4d35204be38dd374c64f8d2233a6fa560a7919
Ji In Lee <loveted0225@naver.com>
2022-01-20 17:00:28 +0900

    feat(Application): 자동차 게임 시작 부분 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,7 +5,11 @@ import racingcar.controller.Game;
 public class Application {
 
     public static void main(String[] args) {
+<<<<<<< HEAD
         Game game = new Game();
+=======
+        Game game= new Game();
+>>>>>>> 73ee422b1 (feat(Application): 자동차 게임 시작 부분 구현)
         game.play();
     }
 }
