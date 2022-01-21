e0132c095d8ef7d23b190646b03b4bae3a417925
fpal95 <snc7303@gmail.com>
2022-01-21 17:07:10 +0900

    fix: 무작위 값을 final변수로 수정

--- src/main/java/racingcar/util/RandomGenerator.java
+++ src/main/java/racingcar/util/RandomGenerator.java
@@ -17,9 +17,14 @@ public class RandomGenerator {
 =======
 
     public int generateRandomNumber() {
+        private static final int maxNumber = 10;
         Random random = new Random();
+<<<<<<< HEAD
 >>>>>>> 715c9de31 (style: 구글 자바 컨벤션 수정)
         return random.nextInt(9);
 >>>>>>> 6d341c991 (feat(RandomGenerator): 랜덤값 생성 구현)
+=======
+        return random.nextInt(maxNumber);
+>>>>>>> 6f445e288 (fix: 무작위 값을 final변수로 수정)
     }
 }
