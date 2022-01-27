b9aa483cd0d4c091dd4bcc6f4691d2480beeeb5a
fpal95 <snc7303@gmail.com>
2022-01-27 11:36:03 +0900

    Fix(CarTest): 자동차 이름길이 테스트 수정

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,7 @@
 package racingcar;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 import org.junit.jupiter.api.Assertions;
@@ -11,6 +12,11 @@ import static org.assertj.core.api.Java6Assertions.assertThat;
 import java.util.Arrays;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 >>>>>>> 226cb95c4 (Test(carTest): 이름길이 및 랜덤값에 따른 position 테스트)
+=======
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+>>>>>>> ef73f901b (Fix(CarTest): 자동차 이름길이 테스트 수정)
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -34,14 +40,13 @@ public class CarTest {
     void 자동차_전진(int randomNumber) {
 =======
     void 이름길이() {
-        Car car = new Car("wooooni");
-
-        assertThatIllegalArgumentException()
-            .isThrownBy((ThrowingCallable) car);
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Car car = new Car("wooooni");
+        });
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {4,8,9})
+    @ValueSource(ints = {4, 8, 9})
     @DisplayName("4이상일 경우 전진")
     void 전진(int randomNumber) {
 >>>>>>> 226cb95c4 (Test(carTest): 이름길이 및 랜덤값에 따른 position 테스트)
@@ -52,11 +57,15 @@ public class CarTest {
 
     @ParameterizedTest
 <<<<<<< HEAD
+<<<<<<< HEAD
     @ValueSource(ints = {0, 1, 3})
     @DisplayName("랜덤값이 4미만일 경우 자동차는 정지한다.")
     void 자동차_정지(int randomNumber) {
 =======
     @ValueSource(ints = {0,1,3})
+=======
+    @ValueSource(ints = {0, 1, 3})
+>>>>>>> ef73f901b (Fix(CarTest): 자동차 이름길이 테스트 수정)
     @DisplayName("4미만일 경우 정지")
     void 정지(int randomNumber) {
 >>>>>>> 226cb95c4 (Test(carTest): 이름길이 및 랜덤값에 따른 position 테스트)
